7c3b6c88b46754ba7d8fc96231eef1d767c9006b
Serg <sgserg94@gmail.com>
2024-05-18 16:00:47 +0300

    add test

---
+++ src/test/java/ru/yandex/AccordionTest.java
@@ -0,0 +1,61 @@
+package ru.yandex;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import ru.yandex.page.MainPage;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+
+public class AccordionTest {
+    private WebDriver webDriver;
+    private int index;
+    private String answer;
+
+
+    public AccordionTest(int index, String answer) {
+        this.index = index;
+        this.answer = answer;
+    }
+
+
+    @Parameterized.Parameters (name = "Номер вопроса {0}, Ответ {1}")
+    public static Object[][] data() {
+        return new Object[][] {
+                {0, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {1, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {4, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {6, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {7, "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
+        };
+    }
+
+    @Before
+    public void setup() {
+        webDriver = WebDriverFactory.getWebDriver(System.getProperty("browser", "firefox"));
+
+        webDriver.get("https://qa-scooter.praktikum-services.ru/");
+    }
+
+    @Test
+    public void accordionTest() {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.closeCookiesWindow();
+        mainPage.expandQuestion(index);
+        boolean answerIsDisplayed = mainPage.answerIsDisplayed(answer);
+        assertTrue("Ответы на вопросы совпадают",answerIsDisplayed);
+        }
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+    }
+}
---
+++ src/test/java/ru/yandex/OrderTest.java
@@ -0,0 +1,97 @@
+package ru.yandex;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import ru.yandex.page.LeasePage;
+import ru.yandex.page.MainPage;
+import ru.yandex.page.OrderPage;
+import static org.junit.Assert.assertTrue;
+
+public class OrderTest {
+    private WebDriver webDriver;
+
+    @Before
+    public void setup() {
+        webDriver = WebDriverFactory.getWebDriver(System.getProperty("browser", "firefox"));
+
+        webDriver.get("https://qa-scooter.praktikum-services.ru/");
+    }
+
+    @Test
+    public void orderNotFound() {
+
+        MainPage mainPage = new MainPage(webDriver);
+
+        mainPage.clickOrderStatusButton();
+
+        mainPage.enterOrderNumber("3565448");
+
+        mainPage.clickGoButton();
+
+        assertTrue("Заказ не найден",mainPage.notFoundImageIsDisplayed());
+
+    }
+
+    @Test
+    public void orderSuccessUpPage() {
+
+        MainPage mainPage = new MainPage(webDriver);
+
+        mainPage.clickCreateOrder();
+
+        OrderPage orderPage = new OrderPage(webDriver);
+
+        orderPage.fillCustomerInfo("Имя", "Фамилия", "Адрес", "Арбатская", "89991231212");
+
+        orderPage.clickNextButton();
+
+        LeasePage leasePage = new LeasePage(webDriver);
+
+        leasePage.selectOrderDate("01.01.2025");
+
+        leasePage.selectRentPeriod();
+
+        leasePage.clickCreateOrderButton();
+
+        orderPage.clickYesButton();
+
+        assertTrue("Оформление заказа через верхнюю кнопку ",mainPage.orderSuccessIsDisplayed());
+
+    }
+
+    @Test
+    public void orderSuccessDownPage() {
+
+        MainPage mainPage = new MainPage(webDriver);
+
+        mainPage.clickCreateOrderDownPage();
+
+        OrderPage orderPage = new OrderPage(webDriver);
+
+        orderPage.fillCustomerInfo("Алексей", "Алексеев", "Россия, г. Рязань, Комсомольская ул., д. 10 кв.99", "Арбатская", "81234567898");
+
+        orderPage.clickNextButton();
+
+        LeasePage leasePage = new LeasePage(webDriver);
+
+        leasePage.selectOrderDate("01.12.2024");
+
+        leasePage.selectRentPeriod();
+
+        leasePage.clickCreateOrderButton();
+
+        orderPage.clickYesButton();
+
+        assertTrue("Оформление заказа  через нижнюю кнопку",mainPage.orderSuccessIsDisplayed());
+
+    }
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+    }
+
+    }
+
